0a7d0fca40f623193fbf48441f700b8ff6b7d0a1
Foster <ftc0003@arastudent.ac.nz>
2020-01-02 16:03:35 +1300

    Done

---
+++ src/build/precache-manifest.241ad3fed0e39a2945a1eefa39f8cc70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb659b16d67ed7476c6a4b7277c10a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28784a7764b2c11ce310",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "1840383ec11bcf2a5bab",
+    "url": "/static/js/2.add3d581.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.add3d581.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28784a7764b2c11ce310",
+    "url": "/static/js/main.8c7ce98e.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  }
+]);
